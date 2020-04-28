5636019ee7afb06656e6844a72553d449cdfc5fb
Alex Woersching <ajw006@gmail.com>
2020-04-27 22:46:51 -0700

    Creates C file outline of what MIPS will do
    This commit creates a C file called bc.c which is meant to act as a
    template for how the assembly code should function.
    Also creates .gitignore

---
+++ 2project/src/bc.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    int* price = (int*)malloc(sizeof(int));
+    int* wrap = (int*)malloc(sizeof(int));
+    int* money = (int*)malloc(sizeof(int));
+
+    printf("Welcome to BobCat Candy, home of our famous BobCat Bars!\nPlease enter the price of a BobCat Bar: ");
+    scanf("%d", price);
+    printf("Please enter the number of wrappers needed to exchange for a new bar: ");
+    scanf("%d", wrap);
+    printf("How much money do you have? ");
+    scanf("%d", money);
+    printf("Good! Let me run the number ... \n");
+    if((*money / *price) < 1) printf("With $%d, you will receive a maximum of 0 Bobcat Bars!\n", *money);
+    else{
+        printf("You first buy %d bars.\n", (*money / *price));
+        printf("With $%d, you will receive a maximum of %d Bobcat Bars!\n", *money, recursion((*money / *price), *wrap, (*money / *price)));
+        
+    }
+    return 0;
+}
+
+int recursion(int wraps, int wex, int total){
+    if(wraps/wex != 0){
+        printf("Then, you will get another %d bars.\n", wraps/wex);
+        total += wraps/wex;
+        recursion(wraps/wex, wex, total);
+    }
+    else return total;
+}
