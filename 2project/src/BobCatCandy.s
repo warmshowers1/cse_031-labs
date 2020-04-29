8cf6d2efa64d692cb5a6a254d216bfb9ac8f6742
Alex Woersching <ajw006@gmail.com>
2020-04-29 03:06:57 -0700

    Creates functioning MIPS file and documents tests
    Creates a functioning assembly file that seems to be fulfilling every
    requirement this program is meant to fulfill.
    Documents the outputs of test cases that were calculated before hand to
    check their legitimacy. All test cases seemed to pass.

--- 2project/src/BobCatCandy.s
+++ 2project/src/BobCatCandy.s
@@ -1,62 +1,141 @@
-.data 
-str0: .asciiz "Welcome to BobCat Candy, home to the famous BobCat Bars!"
-# Declare any necessary data here
-
-
-
+	.data 
+str0: .asciiz "Welcome to BobCat Candy, home to the famous BobCat Bars!\nPlease enter the price of a Bobcat Bar: "
+qwraps: .asciiz "Please enter the number of wrappers needed to exchange for a new bar: "
+qmoney: .asciiz "How much money do you have?: "
+calc: .asciiz "Good! Let me run the number ... \n"
+nope: .asciiz ", you will receive a maximum of 0 Bobcat Bars!\n"
+first: .asciiz "You first buy "
+then: .asciiz "Then, you will get another "
+bars: .asciiz " bars.\n"
+end1: .asciiz "With $"
+end2: .asciiz ", you will receive a maximum of "
+end3: .asciiz " Bobcat Bars!\n"
 .text
 
-main:
-		#This is the main program.
-		#It first asks user to enter the price of each BobCat Bar.
-		#It then asks user to enter the number of bar wrappers needed to exchange for a new bar.
-		#It then asks user to enter how much money he/she has.
-		#It then calls maxBars function to perform calculation of the maximum BobCat Bars the user will receive based on the information entered. 
-		#It then prints out a statement about the maximum BobCat Bars the user will receive.
-		
-		addi $sp, $sp -4	# Feel free to change the increment if you need for space.
-		sw $ra, 0($sp)
+main:		addi $sp, $sp -4	# Feel free to change the increment if you need for space.
+		sw $ra, 4($sp)
 		# Implement your main here
 
-
-
-
-
+		# Gets user input and stores the values in appropriate registers
+		la $a0, str0
+		li $v0, 4
+		syscall
+		li $v0, 5
+		syscall
+		move $a1, $v0
+		la $a0, qwraps
+		li $v0, 4
+		syscall
+		li $v0, 5
+		syscall
+		move $a2, $v0
+		la $a0, qmoney
+		li $v0, 4
+		syscall
+		li $v0, 5
+		syscall
+		move $a3, $v0
+		# $a1 = price
+		# $a2 = n
+		# $a3 = money
+		la $a0, calc
+		li $v0, 4
+		syscall
+		
 		jal maxBars 	# Call maxBars to calculate the maximum number of BobCat Bars
-
+		
 		# Print out final statement here
-
-
+		la $a0, end1
+		li $v0, 4
+		syscall
+		move $a0, $a3
+		li $v0, 1
+		syscall
+		la $a0, end2
+		li $v0, 4
+		syscall
+		move $a0, $v1		# Notice I'm storing the final value in $v1, not $v0
+		li $v0, 1
+		syscall
+		la $a0, end3
+		li $v0, 4
+		syscall
 
 		j end			# Jump to end of program
 
 
 
-maxBars:
+maxBars:	addi $sp, $sp, -4
+		sw $ra, 4($sp)
 		# This function calculates the maximum number of BobCat Bars.
-		# It takes in 3 arguments ($a0, $a1, $a2) as n, price, and money. It returns the maximum number of bars
-
-
-
+		# It takes in 3 arguments ($a1, $a2, $a3) as price, n, and money. It returns the maximum number of bars
+		div $a3, $a1
+		mflo $a1
+		# a1 = Bars that can be BOUGHT
+		# a2 = n
+		blez $a1, nobuy # Jump to nobuy if they don't have enough money
+		sw $a1, 0($sp)
+		
+		la $a0, first
+		li $v0, 4
+		syscall
+		move $a0, $a1
+		li $v0, 1
+		syscall
+		la $a0, bars
+		li $v0, 4
+		syscall
+		
+		
 		jal newBars 	# Call a helper function to keep track of the number of bars.
 		
-
+		lw $t0, 0($sp)
+		add $v1, $v1, $t0
 		
+		lw $ra, 4($sp)
 		jr $ra
 		# End of maxBars
 
-newBars:
+newBars:	addi $sp, $sp -12
+		sw $ra, 8($sp)
 		# This function calculates the number of BobCat Bars a user will receive based on n.
-		# It takes in 2 arguments ($a0, $a1) as number of wrappers left so far and n.
-
+		# It takes in 2 arguments ($a1, $a2) as number of wrappers left so far and n.
+		div $a1, $a2
+		mflo $a1
+		beq $a1, $0, pop
 		
+		la $a0, then
+		li $v0, 4
+		syscall
+		move $a0, $a1
+		li $v0, 1
+		syscall
+		la $a0, bars
+		li $v0, 4
+		syscall
 		
+		sw $a1, 4($sp)
+		jal newBars
 		
-		
-		
-		jr $ra
+		lw $t0, 4($sp)
+		add $v1, $v1, $t0
 		# End of newBars
+		
+pop:		lw $ra, 8($sp)
+		addi $sp, $sp, 12
+		jr $ra
 
+nobuy:		# "With $ [$a3], you will receive a maximum of 0 Bobcat bars!"
+		la $a0, end1
+		li $v0, 4
+		syscall
+		move $a0, $a3
+		li $v0, 1
+		syscall
+		la $a0, nope
+		li $v0, 4
+		syscall
+		j end
 end: 
 		# Terminating the program
 		lw $ra, 0($sp)
